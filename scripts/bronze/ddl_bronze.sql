af0a15b92d6bfed8e1db679eb6669e07b0d1a886
Kieryn Beyerl <kieryn.beyerl@gmail.com>
2025-07-21 11:36:30 -0500

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,63 @@
+IF OBJECT_ID ('bronze.subway_ridership', 'U') IS NOT NULL
+	DROP TABLE bronze.subway_ridership;
+GO
+
+CREATE TABLE bronze.subway_ridership (
+	transit_timestamp NVARCHAR(50),
+	tansit_mode NVARCHAR(50),
+	station_complex_id NVARCHAR(50),
+	station_complex NVARCHAR(255),
+	borough NVARCHAR(255),
+	payment_method NVARCHAR(255),
+	fare_class_category NVARCHAR(255),
+	ridership INT,
+	transfers INT,
+	latitude DECIMAL(8,6),
+	longitude DECIMAL(9,6),
+	Georeference NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID ('bronze.stations_and_complexes', 'U') IS NOT NULL
+	DROP TABLE bronze.stations_and_complexes;
+GO
+
+CREATE TABLE bronze.stations_and_complexes (
+	complex_id INT,
+	is_complex NVARCHAR(50),
+	no_station_complex INT,
+	stop_name NVARCHAR(255),
+	display_name NVARCHAR(255),
+	constituent_station_names NVARCHAR(255),
+	station_ids NVARCHAR(50),
+	gtfs_stop_ids NVARCHAR(50),
+	borough NVARCHAR(50),
+	cbd NVARCHAR(50),
+	daytime_routes NVARCHAR(50),
+	structure_type NVARCHAR(50),
+	latitude DECIMAL (8,6),
+	longitude DECIMAL (9,6),
+	ada INT,
+	ada_notes NVARCHAR(255) NULL
+);
+GO
+
+IF OBJECT_ID ('bronze.jan_ridership_2024', 'U') IS NOT NULL
+	DROP TABLE bronze.jan_ridership_2024;
+GO
+
+CREATE TABLE bronze.jan_ridership_2024 (
+	transit_timestamp NVARCHAR(50),
+	tansit_mode NVARCHAR(50),
+	station_complex_id NVARCHAR(50),
+	station_complex NVARCHAR(255),
+	borough NVARCHAR(255),
+	payment_method NVARCHAR(255),
+	fare_class_category NVARCHAR(255),
+	ridership INT,
+	transfers INT,
+	latitude DECIMAL(8,6),
+	longitude DECIMAL(9,6),
+	Georeference NVARCHAR(50)
+);
+GO
