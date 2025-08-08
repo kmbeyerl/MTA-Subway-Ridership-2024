6119c795b167c2149bfd7b66b42341df010ad987
Kieryn Beyerl <kieryn.beyerl@gmail.com>
2025-08-08 15:28:52 -0500

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,14 @@
+/*
+===================================================================================
+DDL Script: Create Silver Tables
+===================================================================================
+Script Purpose:
+  This script creates tables in the 'silver' schema, dropping existing tables
+  if they already exist.
+  Run this script to re-define the DDL structure of 'silver' tables
+===================================================================================
+*/
+
 IF OBJECT_ID ('silver.subway_ridership', 'U') IS NOT NULL
 	DROP TABLE silver.subway_ridership;
 GO
